40b23270ea49104ac8732b648c632efd9f4452f1
TheTinyThinker <125364418+TheTinyThinker@users.noreply.github.com>
2025-04-01 12:45:12 +0100

    armoured cors file

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,9 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:5173'),'https://faywooquizzes.web.app'
+'allowed_origins' => [
+    env('FRONTEND_URL', 'http://localhost:5173'),
+    'https://faywooquizzes.web.app'
 ],
 
     'allowed_origins_patterns' => [],
