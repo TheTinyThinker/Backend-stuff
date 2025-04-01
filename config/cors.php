366e9bad47d29206719566c03d0022fb6731bb7b
TheTinyThinker <125364418+TheTinyThinker@users.noreply.github.com>
2025-04-01 12:32:46 +0100

    grz

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,8 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:5173', 'https://faywooquizzes.web.app')],
+    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:5173'),'https://faywooquizzes.web.app'
+],
 
     'allowed_origins_patterns' => [],
 
