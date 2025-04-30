6c0b8ab38c6e37bb454562bfccc49d1e398c30ac
TheTinyThinker <125364418+TheTinyThinker@users.noreply.github.com>
2025-04-30 13:23:46 +0100

    8765432

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,14 +23,14 @@ class DatabaseSeeder extends Seeder
             // Create users with predefined credentials for testing
             $adminUser = User::create([
                 'name' => 'Admin User',
-                'email' => 'FayWooAdmin123@faywoo.com',
+                'email' => 'FayWooAdmin1234@faywoo.com',
                 'password' => Hash::make('password123'),
                 // Removed is_admin flag
             ]);
 
             $testUser = User::create([
                 'name' => 'Test User',
-                'email' => 'FayWooUser123@faywoo.com',
+                'email' => 'FayWooUser1234@faywoo.com',
                 'password' => Hash::make('password123'),
             ]);
 
