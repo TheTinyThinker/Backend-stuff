11d0b3a14cdd4c0a92f2e229eeca87e8ae7fe5db
TheTinyThinker <125364418+TheTinyThinker@users.noreply.github.com>
2025-04-30 13:13:35 +0100

    server database and filesystems changes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,14 +23,14 @@ class DatabaseSeeder extends Seeder
             // Create users with predefined credentials for testing
             $adminUser = User::create([
                 'name' => 'Admin User',
-                'email' => 'FayWooAdmin12@faywoo.com',
+                'email' => 'FayWooAdmin123@faywoo.com',
                 'password' => Hash::make('password123'),
                 // Removed is_admin flag
             ]);
 
             $testUser = User::create([
                 'name' => 'Test User',
-                'email' => 'FayWooUser12@faywoo.com',
+                'email' => 'FayWooUser123@faywoo.com',
                 'password' => Hash::make('password123'),
             ]);
 
